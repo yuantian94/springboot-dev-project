7c8f04b4fcbccb6011ccc5364ac4bea30162545c
Yuan Tian <michealty94@gmail.com>
2023-12-03 20:41:14 -0600

    Component Auto Scanning (within the package where application file is)

---
+++ 02-spring-boot-spring-core/02-ccomponent-scanning/src/main/java/com/project/springcoredemo/common/Coach.java
@@ -0,0 +1,5 @@
+package com.project.springcoredemo.common;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/02-ccomponent-scanning/src/main/java/com/project/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.project.springcoredemo.common;
+
+import com.project.springcoredemo.common.Coach;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+}
