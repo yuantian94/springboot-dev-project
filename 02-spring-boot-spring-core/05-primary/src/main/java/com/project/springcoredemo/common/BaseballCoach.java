a95477e5133a32f8300bc0c7da3068a5a5ae5497
Yuan Tian <michealty94@gmail.com>
2023-12-05 00:44:21 -0600

    Practice using primary annotation on one bean when multiple beans available(qualifer has higher priority when mixed with primary)

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/project/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,13 @@
+package com.project.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class BaseballCoach implements Coach{
+    @Override
+    public String getDailyWorkout(){
+        return "Spend 30 minutes in batting practice";
+    }
+}
