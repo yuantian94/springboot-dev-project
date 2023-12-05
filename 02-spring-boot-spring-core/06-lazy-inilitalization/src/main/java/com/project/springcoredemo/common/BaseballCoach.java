51d6c2109d55cd544e06792157959f1b89218b02
Yuan Tian <michealty94@gmail.com>
2023-12-05 00:57:45 -0600

    Practice using lazy initialization, using @Lazy for individual bean, using spring.main.lazy-initialization=true in properties file for global setting

---
+++ 02-spring-boot-spring-core/06-lazy-inilitalization/src/main/java/com/project/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,16 @@
+package com.project.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class BaseballCoach implements Coach{
+    public BaseballCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkout(){
+        return "Spend 30 minutes in batting practice";
+    }
+}
