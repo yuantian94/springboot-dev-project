6f220bc3f3ad195bac7162c86668b8dbbdc5923d
Yuan Tian <michealty94@gmail.com>
2023-12-05 20:13:03 -0600

    Pratice using Bean lifecycle methods (For prototype scoped beans, Spring does not call the destroy method. Prototype beans are lazy by default. There is no need to use the @Lazy annotation for prototype scopes beans.)

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle/src/main/java/com/project/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,33 @@
+package com.project.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class BaseballCoach implements Coach{
+    public BaseballCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    //define our init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    //define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In doMyCleanupStuff() " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout(){
+        return "Spend 30 minutes in batting practice";
+    }
+}
---
+++ 02-spring-boot-spring-core/08-bean-lifecycle/src/main/java/com/project/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.project.springcoredemo.rest;
+
+import com.project.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define a private field for the dependency
+    private Coach myCoach;
+
+
+    // define a constructor for dependency injection
+    @Autowired
+    public DemoController(@Qualifier("baseballCoach") Coach theCoach){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
