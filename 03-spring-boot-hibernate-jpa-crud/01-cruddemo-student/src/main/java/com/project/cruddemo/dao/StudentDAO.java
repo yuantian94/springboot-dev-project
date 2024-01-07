cca8a98af66d9e4836a4d02d864c4d55dfd06023
Yuan Tian <michealty94@gmail.com>
2024-01-07 17:34:04 -0600

    Practice to retrieve/read the student object in the MySQL database: 1. add new method to DAO interface 2. Add new method to DAO implementation 3. Update the main app

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/project/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/project/cruddemo/dao/StudentDAO.java
@@ -4,4 +4,6 @@ import com.project.cruddemo.entity.Student;
 
 public interface StudentDAO {
     void save(Student theStudent);
+
+    Student findById(Integer id);
 }
--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/project/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/project/cruddemo/dao/StudentDAOImpl.java
@@ -22,4 +22,11 @@ public class StudentDAOImpl implements StudentDAO{
     public void save(Student theStudent) {
         entityManager.persist(theStudent);
     }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+
 }
