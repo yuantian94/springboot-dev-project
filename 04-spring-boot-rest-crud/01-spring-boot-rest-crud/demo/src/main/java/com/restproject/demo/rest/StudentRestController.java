e5a8c0f5b92937865a9f161c96a4da8f06187e06
Yuan Tian <michealty94@gmail.com>
2024-01-11 23:16:33 -0600

    practice to develop a rest api that return a student by ID using @PathVariable, ID is index of the student list to keep it simple.

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/restproject/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/restproject/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.restproject.demo.rest;
 
 import com.restproject.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,18 +13,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> theStudents;
 
-    // define endpoint for "/students" - return a list of students
-
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-
-        List<Student> theStudents = new ArrayList<>();
+    // define @PostConstruct to load the student data ... only once!
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Mike","Tian"));
         theStudents.add(new Student("Jingyan","Huang"));
         theStudents.add(new Student("Niuniu","Tian"));
+    }
+    // define endpoint for "/students" - return a list of students
 
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return theStudents;
+    }
+
+    // define endpoint or "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        // just index into the list .. keep it simple for now
+        return theStudents.get(studentId);
     }
 }
