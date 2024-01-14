625f35ec8919d9768b9727b4dc35e187b04a8ac6
Yuan Tian <michealty94@gmail.com>
2024-01-13 21:29:57 -0600

    Practice to use Spring Data JPA to simplify DAO codes for different entities. All basic API DAO methods are provided by the interface JpaRepository. Steps: 1. Extend JpaRepository interface. 2. Use Repository in your Service implementation

---
+++ 04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/cruddemo/src/main/java/com/RestAPI/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.RestAPI.cruddemo.dao;
+
+import com.RestAPI.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+}
