113b91509ad338b35e76d7692f2b125deaa383bc
Yuan Tian <michealty94@gmail.com>
2024-01-13 22:26:21 -0600

    Practice to define custom path name, change page size and use sort functionionality

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/cruddemo/src/main/java/com/RestAPI/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/cruddemo/src/main/java/com/RestAPI/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.RestAPI.cruddemo.dao;
 
 import com.RestAPI.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
