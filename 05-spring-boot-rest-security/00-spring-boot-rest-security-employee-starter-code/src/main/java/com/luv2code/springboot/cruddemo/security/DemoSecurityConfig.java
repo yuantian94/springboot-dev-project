661a56c07a2869afae739476d3c8e49040068708
Yuan Tian <michealty94@gmail.com>
2024-01-15 23:10:05 -0600

    Practice to shift user credentials and roles from in-memory to database. Steps: 1. Develop SQL Script to setup database tables 2. Add Database Support to Maven POM file 3. Create JDBC connection properties in application.properties file 4. Update Spring Security config file to use JDBC

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -8,10 +8,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails john = User.builder()
@@ -33,6 +38,15 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john,mary,susan);
+    }
+    */
+
+
+    // add support for JDBC ... no more hardcoded users
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
