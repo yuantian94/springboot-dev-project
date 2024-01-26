b4e02716f9e11892f026efb374eba80542391889
Yuan Tian <michealty94@gmail.com>
2024-01-26 00:23:04 -0600

    For a simple application, @RequestMapping() handles all HTTP methods: GET, POST... But in some circumenstances we want to be clear on which HTTP method we are dealing with using @GetMapping, @PostMapping....Use POST for message with over 1000 characters in length or binary data

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloword/src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloword/src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -3,6 +3,8 @@ package com.springboot.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloWorldController {
 
     // need a controller method to show initial HTML form
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
@@ -35,7 +37,7 @@ public class HelloWorldController {
         return "helloworld";
     }
 
-    @RequestMapping("/processFormVersionThree")
+    @PostMapping("/processFormVersionThree")
     public String processFormVersionThree(@RequestParam("studentName") String theName, Model model){
         // convert the data to all caps
         theName = theName.toUpperCase();
