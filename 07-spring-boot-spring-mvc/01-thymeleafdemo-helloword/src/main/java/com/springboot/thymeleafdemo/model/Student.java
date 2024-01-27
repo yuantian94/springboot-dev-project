336972a0a5789517fd8cb14aeb3e495743e89026
Yuan Tian <michealty94@gmail.com>
2024-01-27 15:53:39 -0600

    Pratice to use Spring MVC Form data binding on radio buttons. Steps: 1. Update HTML form 2. Update Student class - add getter/setter for new property 3. Update confirmation page

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloword/src/main/java/com/springboot/thymeleafdemo/model/Student.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloword/src/main/java/com/springboot/thymeleafdemo/model/Student.java
@@ -6,6 +6,8 @@ public class Student {
 
     private String country;
 
+    private String favoriteLanguage;
+
     public Student(){
 
     }
@@ -32,5 +34,13 @@ public class Student {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
     }
 }
