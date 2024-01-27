9fb1208c29fb8786e367780308601e34a6927a8b
Yuan Tian <michealty94@gmail.com>
2024-01-27 16:37:40 -0600

    Pratice to use Spring MVC Form data binding on Check Boxes. Steps: 1. Update HTML form 2. Update Student class - add getter/setter for new property 3. Update confirmation page

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloword/src/main/java/com/springboot/thymeleafdemo/model/Student.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloword/src/main/java/com/springboot/thymeleafdemo/model/Student.java
@@ -1,5 +1,7 @@
 package com.springboot.thymeleafdemo.model;
 
+import java.util.List;
+
 public class Student {
     private String firstName;
     private String lastName;
@@ -7,6 +9,8 @@ public class Student {
     private String country;
 
     private String favoriteLanguage;
+
+    private List<String> favoriteSystems;
 
     public Student(){
 
@@ -42,5 +46,13 @@ public class Student {
 
     public void setFavoriteLanguage(String favoriteLanguage) {
         this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public List<String> getFavoriteSystems() {
+        return favoriteSystems;
+    }
+
+    public void setFavoriteSystems(List<String> favoriteSystems) {
+        this.favoriteSystems = favoriteSystems;
     }
 }
