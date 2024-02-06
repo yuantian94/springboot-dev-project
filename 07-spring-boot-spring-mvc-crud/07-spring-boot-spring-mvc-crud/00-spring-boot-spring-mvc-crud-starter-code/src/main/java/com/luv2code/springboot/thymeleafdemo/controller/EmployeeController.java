2e7cca9e263f5fe475c732b8748b5bd13b021484
Yuan Tian <michealty94@gmail.com>
2024-02-06 00:44:49 -0600

    Add the function to delete an existing employee: 1. Add 'Delete' button/link on page 2. Add controller code for 'Delete'

--- 07-spring-boot-spring-mvc-crud/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -56,4 +56,11 @@ public class EmployeeController {
 
         return "employees/employee-form";
     }
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId){
+        // delete the employee
+        employeeService.deleteById(theId);
+        // redirect to the /employees/list
+        return "redirect:/employees/list";
+    }
 }
