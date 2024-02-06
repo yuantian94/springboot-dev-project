c31f3e0ff5bd44031b583f5019c0b0a46b2f230a
Yuan Tian <michealty94@gmail.com>
2024-02-05 23:28:05 -0600

    Add the function to add an employee: 1. New Add Employee button for list-employees.html(request mapping /employees/showFormForAdd and controller, before you show the form, you must add a model attribute, this is an object that will hold form data for the data binding. Thymeleaf has special expressions for binding Spring MVC form data, automatically setting/retrieving data from a java object, th:action(Location to send form data), th:object(reference to model attribute), th:field(Bind input field to a propertyon model attribute)) 2. Create HTML form for new employeeThymeleaf has special expressions for binding Spring MVC form  3. Process form data to save employee

--- 07-spring-boot-spring-mvc-crud/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -26,6 +28,24 @@ public class EmployeeController {
         List<Employee> theEmployees = employeeService.findAll();
         // add to the spring model
         theModel.addAttribute("employees", theEmployees);
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public  String showFormForAdd(Model theModel){
+        // Create model attribute to bind form data
+        Employee theEmployee = new Employee();
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+        // save the employee
+        employeeService.save(theEmployee);
+
+        // use a redirect to prevent duplicate submission
+        return "redirect:/employees/list";
     }
 }
