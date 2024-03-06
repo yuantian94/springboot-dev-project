b3bf1d16e4557cea5b7f150df4d09e8d2dbb9cc8
Yuan Tian <michealty94@gmail.com>
2024-03-05 22:46:23 -0600

    Spring MVC Security - Custom Login Form - 1. Modify Spring Security Configuration to reference custom login form (using SecurityFilterChain Bean). 2. Develop a Controller to show the custom login form (this is for loginPage(/showMyLoginPage), the loginProcessingUrl() is handled by spring boot security filter module automaticly). 3. Create custom login form (*the advanatge to use context path in HTML file)

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/controller/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/controller/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.springboot.demosecurity.controller.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -27,5 +29,20 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john,mary,susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                    configurer
+                            .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                );
+        return http.build();
     }
 }
