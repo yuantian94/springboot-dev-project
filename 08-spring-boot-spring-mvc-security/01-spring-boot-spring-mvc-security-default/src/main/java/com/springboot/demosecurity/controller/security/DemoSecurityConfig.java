42789953ddc3b4aaa086297734f30924250f6f62
Yuan Tian <michealty94@gmail.com>
2024-03-07 12:58:29 -0600

    Spring MVC Security - Restrict URLs Based on Roles - 1. Create supporting controller code and view pages. 2. Restrict Access based on roles <update Java configuration file. General Syntax: requestMatchers(<< add path to match on >>).hasRole(<< authorized role >>)>

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/controller/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/springboot/demosecurity/controller/security/DemoSecurityConfig.java
@@ -35,6 +35,9 @@ public class DemoSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(configurer ->
                     configurer
+                            .requestMatchers("/").hasRole("EMPLOYEE")
+                            .requestMatchers("/leaders/**").hasRole("MANAGER")
+                            .requestMatchers("/systems/**").hasRole("ADMIN")
                             .anyRequest().authenticated()
                 )
                 .formLogin(form ->
