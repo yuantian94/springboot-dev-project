9f84177b1789e838b831a44229f23863d52efd3f
Yuan Tian <michealty94@gmail.com>
2024-03-14 16:59:21 -0500

    JPA/Hibernate Advanced Mappings - OneToMany - Fetch Types:Eager vs Lazy - Default Fetch Types: 1to1-Eager, 1toM-Lazy, Mto1-Eager, MtoM-Lazy - Lazy loading requires a valid Hibernate session. Practice to load course info along with instructor

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/cruddemo/src/main/java/com/project/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/cruddemo/src/main/java/com/project/cruddemo/CruddemoApplication.java
@@ -26,8 +26,19 @@ public class CruddemoApplication {
 			// findInstructorDetail(appDAO);
 			// deleteInstructorDetail(appDAO);
 
-			createInstructorWithCourses(appDAO);
+			// createInstructorWithCourses(appDAO);
+			findInstructorWithCourses(appDAO);
 		};
+	}
+
+	private void findInstructorWithCourses(AppDAO appDAO) {
+		int theId=1;
+		System.out.println("Finding instructor id: " + theId);
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated courses: " + tempInstructor.getCourses());
+		System.out.println("Done");
 	}
 
 	private void createInstructorWithCourses(AppDAO appDAO) {
