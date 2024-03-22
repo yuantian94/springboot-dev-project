8cca3f2a93d955f72fbaf28f6371bb8ca21391cb
Yuan Tian <michealty94@gmail.com>
2024-03-22 18:54:57 -0500

    AOP - @AfterReturning Advice - Modify Return Value - basically intercept the returned data and modify it, and pass to return.

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aopdemo/src/main/java/com/project/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aopdemo/src/main/java/com/project/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -29,8 +29,24 @@ public class MyDemoLoggingAspect {
         System.out.println("\n=====> Executing @AfterReturning on method: " + method);
         // print out the results of the method call
         System.out.println("\n=====> result is: " + result);
+
+        // let's post-process the data .. let's modify it
+
+        // convert the account names to uppercase
+        convertAccountNamesToUpperCase(result);
+
+        System.out.println("\n=====> result is: " + result);
     }
 
+    private void convertAccountNamesToUpperCase(List<Account> result){
+        // loop through accounts
+        for(Account tempAccount : result) {
+            // get uppercase version of name
+            String theUpperName = tempAccount.getName().toUpperCase();
+            // update the name on the account
+            tempAccount.setName(theUpperName);
+        }
+    }
     // this is where we add all our related advices for logging
     // let's start with an @Before advice
     @Before("com.project.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
