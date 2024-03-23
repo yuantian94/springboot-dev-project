d03a7af50d02c1b9b81cb2acd386871f32a72559
Yuan Tian <michealty94@gmail.com>
2024-03-23 00:16:48 -0500

    AOP - @Around Advice - Handle Exception - we can handle the exception in our aspect so that the main function doesn't know there was an exception.

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aopdemo/src/main/java/com/project/aopdemo/service/TrafficFortuneService.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aopdemo/src/main/java/com/project/aopdemo/service/TrafficFortuneService.java
@@ -2,4 +2,6 @@ package com.project.aopdemo.service;
 
 public interface TrafficFortuneService {
     String getFortune();
+
+    String getFortune(boolean tripWire);
 }
--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aopdemo/src/main/java/com/project/aopdemo/service/TrafficFortuneServiceImpl.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aopdemo/src/main/java/com/project/aopdemo/service/TrafficFortuneServiceImpl.java
@@ -19,4 +19,12 @@ public class TrafficFortuneServiceImpl implements TrafficFortuneService{
 
         return "Expect heavy traffic this morning";
     }
+
+    @Override
+    public String getFortune(boolean tripWire) {
+        if(tripWire){
+            throw new RuntimeException("Major accident! Highway is closed!");
+        }
+        return getFortune();
+    }
 }
